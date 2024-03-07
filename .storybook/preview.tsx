6d45abcf5c409e2585e5bc82f3d0300ee6fa1cce
Lance Gliser <lance.gliser@howso.com>
2024-03-07 14:00:58 -0600

    Setup proper light dark mode switching

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,11 +7,29 @@ import { MemoryRouter } from "react-router-dom";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
 import "../src/tailwind.css";
+import { standardFlowbiteTheme } from "../src/themes/index";
+import { Flowbite } from "flowbite-react";
+
+const light = {
+  name: "Light",
+  value: "#fff",
+};
+
+const dark = {
+  name: "Dark",
+  value: "#000",
+};
 
 const preview: Preview = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
     // layout: "fullscreen", // TODO not having any effect..
+    backgrounds: {
+      // Load them by hand, or make a nice loop if you wish
+      values: [light, dark],
+      // Ensure a default is set, so you avoid type errors reading from undefined!
+      default: light.name,
+    },
     actions: { argTypesRegex: "^on.*" },
     controls: {
       matchers: {
@@ -21,6 +39,19 @@ const preview: Preview = {
     },
   },
   decorators: [
+    (Story: StoryFn, context) => (
+      <Flowbite
+        theme={{
+          theme: standardFlowbiteTheme,
+          mode:
+            context.globals.backgrounds?.value === dark.value
+              ? "dark"
+              : "light",
+        }}
+      >
+        <Story />
+      </Flowbite>
+    ),
     (Story: StoryFn) => (
       <MemoryRouter>
         <Story />
