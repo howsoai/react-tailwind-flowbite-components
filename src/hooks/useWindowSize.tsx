c1135da7380464aabe4f4c2aa7e0687fd25ba199
Lance Gliser <lance.gliser@howso.com>
2024-03-15 10:10:47 -0500

    Added debounce management into useWindowSize

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback, useMemo, useLayoutEffect } from "react";
+import { debounce } from "lodash";
 
 interface WindowSize {
   width: number;
@@ -6,11 +7,21 @@ interface WindowSize {
   ratio: number;
 }
 
+type OmitFirst<T extends any[]> = T extends [infer A, ...infer R] ? R : never;
+type DebounceArgs = OmitFirst<Parameters<typeof debounce>>;
+
+export type UseWindowSizeParams = {
+  /** Lodash debounce args */
+  debounceArgs?: DebounceArgs;
+};
+
 /**
  * Subscribes to changes to the window size as a state object.
- * Subscribers are responsbile for their own deboucing or deffered value implementations.
  */
-export const useWindowSize = (): WindowSize => {
+export const useWindowSize = (
+  /** You must use a static object, or useMemo to avoid infinate render loops. */
+  params: UseWindowSizeParams = {}
+): WindowSize => {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState({
@@ -19,23 +30,30 @@ export const useWindowSize = (): WindowSize => {
     ratio: 1,
   });
 
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-        ratio: Number((window.innerHeight / window.innerWidth).toFixed(2)),
-      });
-    }
+  const _handleResize = useCallback(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      ratio: Number((window.innerHeight / window.innerWidth).toFixed(2)),
+    });
+  }, [setWindowSize]);
+  const handleResize = useMemo(
+    () =>
+      params.debounceArgs
+        ? debounce(_handleResize, ...params.debounceArgs)
+        : _handleResize,
+    [params.debounceArgs, _handleResize]
+  );
+
+  useLayoutEffect(() => {
     // Add event listener
     window.addEventListener("resize", handleResize);
     // Call handler right away so state gets updated with initial window size
-    handleResize();
+    _handleResize();
+
     // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+  }, [handleResize]); // Empty array ensures that effect is only run on mount
 
   return windowSize;
 };
