59901ba12db1afa0a97e74b13adebef5ce7ae40d
Lance Gliser <lance.gliser@howso.com>
2024-03-22 08:59:05 -0500

    Isolated the tailwind configuration into the Storybook folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ import { standardTailwindConfigThemeExtend } from "./src/themes";
 export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./storybook/**/*.{jsx,tsx,html}",
     "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
