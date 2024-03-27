487c2628fc6b7ccf4ea1f7dfdab72c1802911c52
Lance Gliser <lance.gliser@howso.com>
2024-03-27 10:15:12 -0500

    Exposed getLinkProps

--- src/components/Typography/Link/Link.tsx
+++ src/components/Typography/Link/Link.tsx
@@ -1,18 +1,36 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
 import { twMerge } from "tailwind-merge";
+import { NewWindowIcon } from "../../Icons";
 
-export type LinkProps = React.DetailedHTMLProps<
-  React.AnchorHTMLAttributes<HTMLAnchorElement>,
-  HTMLAnchorElement
->;
-export const Link: FC<LinkProps> = function (props) {
-  return (
-    <a
-      {...props}
-      className={twMerge(
-        "cursor-pointer text-blue-600 hover:underline dark:text-blue-500",
-        props.className
-      )}
-    />
-  );
+export type LinkProps = ComponentProps<"a"> & { external?: boolean };
+/**
+ * A generic <a> link for use in non-structural contexts.
+ * If you need to create your own links, most commonly for React Router
+ * use `getLinkProps()`.
+ **/
+export const Link: FC<LinkProps> = (props) => {
+  const { children, ...mergedProps } = getLinkProps(props);
+  return <a {...mergedProps}>{children}</a>;
 };
+
+/**
+ * Returns link props augmented with display standards:
+ * - className
+ * - External icon in children
+ */
+export const getLinkProps = (props: LinkProps): LinkProps => ({
+  className: twMerge(linkClasses, props.className),
+  children: (
+    <>
+      {props.children}
+      {props.external && (
+        <>
+          {" "}
+          <NewWindowIcon />
+        </>
+      )}
+    </>
+  ),
+});
+
+const linkClasses = "text-blue-600 underline dark:text-blue-500";
