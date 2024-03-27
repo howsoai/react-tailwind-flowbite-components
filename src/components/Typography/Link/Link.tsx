89e0a952fc6122c4cadc0d8e7e204249d6e3f754
Lance Gliser <lance.gliser@howso.com>
2024-03-27 10:45:56 -0500

    Setup Link to use inline-flex

--- src/components/Typography/Link/Link.tsx
+++ src/components/Typography/Link/Link.tsx
@@ -2,7 +2,10 @@ import { ComponentProps, FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { NewWindowIcon } from "../../Icons";
 
-export type LinkProps = ComponentProps<"a"> & { external?: boolean };
+export type LinkProps = ComponentProps<"a"> & {
+  /** Indicates a link that will take the off site. */
+  external?: boolean;
+};
 /**
  * A generic <a> link for use in non-structural contexts.
  * If you need to create your own links, most commonly for React Router
@@ -19,18 +22,15 @@ export const Link: FC<LinkProps> = (props) => {
  * - External icon in children
  */
 export const getLinkProps = (props: LinkProps): LinkProps => ({
+  ...props,
   className: twMerge(linkClasses, props.className),
   children: (
     <>
       {props.children}
-      {props.external && (
-        <>
-          {" "}
-          <NewWindowIcon />
-        </>
-      )}
+      {props.external && <NewWindowIcon className="inline-block ml-[.25ch]" />}
     </>
   ),
 });
 
-const linkClasses = "text-blue-600 underline dark:text-blue-500";
+const linkClasses =
+  "inline-flex items-center text-blue-600 underline dark:text-blue-500";
