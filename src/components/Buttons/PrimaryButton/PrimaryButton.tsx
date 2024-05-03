05ee2e3ecf132e8fe6bb88670b0015fd4fe7cd05
Lance Gliser <lance.gliser@howso.com>
2024-05-03 12:57:21 -0500

    Removed the Flowbite forced wrapper around PrimaryButton

--- src/components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,16 +1,11 @@
-import { Button, ButtonProps, Flowbite } from "flowbite-react";
+import { Button, ButtonProps } from "flowbite-react";
 import { FC } from "react";
-import { standardFlowbiteTheme } from "../../../themes";
 
 export type PrimaryButtonProps = ButtonProps;
 /**
- * A convience wrapper setting defaults for Primary buttons and tracking all usages.
+ * A convince wrapper setting defaults for Primary buttons and tracking all usages.
  * By default: Blue, filled
  **/
-export const PrimaryButton: FC<PrimaryButtonProps> = function ({ ...props }) {
-  return (
-    <Flowbite theme={{ theme: standardFlowbiteTheme }}>
-      <Button color="primary" {...props} />
-    </Flowbite>
-  );
+export const PrimaryButton: FC<PrimaryButtonProps> = (props) => {
+  return <Button color="primary" {...props} />;
 };
