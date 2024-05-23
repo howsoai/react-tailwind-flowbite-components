77b30f3e2556849a9017d9b53da4f2e346907fd7
Lance Gliser <lance.gliser@howso.com>
2024-05-23 14:02:27 -0500

    20026: Primary and Secondary button modifications (#27)

--- src/components/Buttons/SecondaryButton/SecondaryButton.tsx
+++ src/components/Buttons/SecondaryButton/SecondaryButton.tsx
@@ -1,11 +1,11 @@
 import { Button, ButtonProps } from "flowbite-react";
 import { FC } from "react";
 
-export type PrimaryButtonProps = ButtonProps;
+export type SecondaryButtonProps = ButtonProps;
 /**
  * A convince wrapper setting defaults for Primary buttons and tracking all usages.
  * By default: Blue, filled
  **/
-export const PrimaryButton: FC<PrimaryButtonProps> = (props) => {
-  return <Button color="primary" {...props} />;
+export const SecondaryButton: FC<SecondaryButtonProps> = (props) => {
+  return <Button color="light" outline {...props} />;
 };
