37d89946c7bc8f0486cc9c7599ad9848f3a035a3
Lance Gliser <lance.gliser@howso.com>
2024-05-30 08:45:24 -0500

    Added ConfigurationIcon

---
+++ src/components/Icons/ConfigurationIcon.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import { IconBaseProps } from "react-icons";
+import { HiAdjustments } from "react-icons/hi";
+
+export const ConfigurationIcon: FC<IconBaseProps> = (props) => {
+  return <HiAdjustments {...props} />;
+};
