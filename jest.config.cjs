c9a585340bca89988dadf9685dd0e4b41d76ca31
Lance Gliser <lance.gliser@howso.com>
2024-06-06 10:02:07 -0500

    Updating jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,4 @@
 module.exports = {
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
-  },
   collectCoverage: false, // See package.json script
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{ts,js,vue}",
@@ -12,5 +6,19 @@ module.exports = {
     "!<rootDir>/node_modules/",
   ],
   coverageReporters: ["text", "cobertura"],
+  moduleFileExtensions: ["js", "ts", "tsx", "json"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  rootDir: "src",
+  setupFiles: [
+    "<rootDir>/../jest/setup/il8n.js",
+    "<rootDir>/../jest/setup/config.js",
+  ],
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+  },
 };
