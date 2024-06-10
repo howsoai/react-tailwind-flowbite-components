e4b77c42bad33804e8c0be6e7641b254cf8a6503
Lance Gliser <lance.gliser@howso.com>
2024-06-10 15:39:24 -0500

    Added public directory to lib for publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import autoprefixer from 'autoprefixer';
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy'
 import pkg from "./package.json" with { type: "json" };
 
 /**
@@ -21,6 +22,11 @@ export default {
       minimize: true,
       modules: true,
     }),
+    copy({
+      targets: [
+        { src: 'public', dest: 'lib' },
+      ]
+    })
     // terser(), // minifies generated bundles
   ],
   external: [
