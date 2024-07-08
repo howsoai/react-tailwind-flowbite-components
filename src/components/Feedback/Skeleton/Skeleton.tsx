b95673dea57ed62a4b5d65422949783bddf4f4ff
Lance Gliser <lance.gliser@howso.com>
2024-07-08 09:41:23 -0500

    20789: MAJOR flowbite-react@0.10.1 (#39)
    - Removed direct react-icons dependency to inherit from flowbite-react
    - Updated MIGRATIONS.md
    - Updated Skeleton component to use span and allow text variant

--- src/components/Feedback/Skeleton/Skeleton.tsx
+++ src/components/Feedback/Skeleton/Skeleton.tsx
@@ -3,27 +3,57 @@ import { twMerge } from "tailwind-merge";
 import { useDefaultTranslation } from "@/hooks";
 
 export type SkeletonProps = {
+  /** Default: "block" */
+  variant?: "block" | "text";
   className?: string;
 };
-export const Skeleton: FC<SkeletonProps> = function (props) {
-  return <BlockSkeleton {...props} />;
+export const Skeleton: FC<SkeletonProps> = ({
+  variant = "block",
+  ...props
+}) => {
+  switch (variant) {
+    case "block":
+      return <BlockSkeleton {...props} />;
+    case "text":
+      return <TextSkeleton {...props} />;
+  }
 };
 
-export interface BlockSkeletonProps {
+export type BlockSkeletonProps = {
   className?: string;
-}
+};
 const BlockSkeleton: FC<BlockSkeletonProps> = ({ className }) => {
   const { t } = useDefaultTranslation();
 
   return (
-    <div
+    <span
       role="status"
       className={twMerge(
-        "min-h-[1lh] rounded animate-pulse bg-gray-200 dark:bg-gray-700",
+        "block min-h-[1lh] rounded animate-pulse bg-gray-200 dark:bg-gray-700",
         className,
       )}
     >
       <span className="sr-only">{t("Feedback.Skeleton.loading")}</span>
-    </div>
+    </span>
+  );
+};
+
+export type TextSkeletonProps = {
+  className?: string;
+};
+const TextSkeleton: FC<BlockSkeletonProps> = ({ className }) => {
+  const { t } = useDefaultTranslation();
+
+  return (
+    <span
+      role="status"
+      className={twMerge(
+        "min-h-[.9lh] rounded animate-pulse bg-gray-200 dark:bg-gray-700",
+        "inline-block align-middle min-w-[1ch]",
+        className,
+      )}
+    >
+      <span className="sr-only">{t("Feedback.Skeleton.loading")}</span>
+    </span>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-import flowbite from "flowbite/plugin";
+import flowbite from "flowbite-react/tailwind";
 import { standardTailwindConfigThemeExtend } from "./src/themes";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    flowbite.content(),
     "./src/**/*.{js,ts,jsx,tsx}",
     "./storybook/**/*.{jsx,tsx,html}",
-    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
-  plugins: [flowbite({ charts: true })],
+  plugins: [flowbite.plugin({ charts: true })],
   theme: {
     extend: standardTailwindConfigThemeExtend,
   },
