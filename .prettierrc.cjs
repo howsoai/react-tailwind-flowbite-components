67dd1e955145548dd5f6746cf910238c5d721841
Lance Gliser <lance.gliser@howso.com>
2024-07-19 08:28:38 -0500

    20872: Modified starting/ending swipe animation to use fixed 25rem units (#43)

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: [
+    "prettier-plugin-tailwindcss"
+  ]
+};
+
+module.exports = config;
