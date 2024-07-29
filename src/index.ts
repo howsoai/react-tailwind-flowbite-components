3e76b6e157520e3e09be8b5c159e12ff25335343
Lance Gliser <lance.gliser@howso.com>
2024-07-29 12:46:00 -0500

    Strongly typing Il8nBundles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Il8nBundle } from "@howso/ui-internationalization-utils";
 import {
   BooleanTextIl8nBundle,
   CodeBlockIl8nBundle,
@@ -6,13 +7,14 @@ import {
   SkeletonIl8nBundle,
 } from "./components";
 import "./main.css";
+import { Languages } from "./constants";
 
 export * from "./components";
 export * from "./hooks";
 export * from "./themes";
 export * from "./utils";
 
-export const Il8nBundles = [
+export const Il8nBundles: Il8nBundle<Languages, any>[] = [
   BooleanTextIl8nBundle,
   CodeBlockIl8nBundle,
   ErrorAlertIl8nBundle,
