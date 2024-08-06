3c52b9acd6790b9885c1d1f7298249e0acfe671c
Lance Gliser <lance.gliser@howso.com>
2024-08-06 09:27:06 -0500

    Added formatBytes function

---
+++ src/utils/files.ts
@@ -0,0 +1,48 @@
+type FormatBytesOptions = {
+  /* Default: 1 */
+  maximumFractionDigits?: number;
+  /* Default: false */
+  scientific?: boolean;
+  /* Default: " " */
+  separator?: string;
+};
+
+/**
+ * Format bytes as human-readable text.
+ *
+ * @param bytes Number of bytes.
+ * @return Formatted string.
+ */
+export const formatBytes = (
+  bytes: number,
+  options: FormatBytesOptions = {},
+): string => {
+  options.maximumFractionDigits ||= 1;
+  options.separator ||= " ";
+
+  const formatter = new Intl.NumberFormat(undefined, {
+    maximumFractionDigits: options.maximumFractionDigits,
+  });
+
+  const thresh = options.scientific ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return formatter.format(bytes) + options.separator + "B";
+  }
+
+  const units = options.scientific
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** options.maximumFractionDigits;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+
+  return formatter.format(bytes) + options.separator + units[u];
+};
