96b3f3d2a910c3c781a04ac8f193e4ed52f752cf
Lance Gliser <lance.gliser@howso.com>
2024-08-06 10:34:47 -0500

    Fixed a test

--- src/utils/files.test.ts
+++ src/utils/files.test.ts
@@ -4,7 +4,7 @@ describe("utils/avatar", () => {
   describe("formatBytes", () => {
     it("should format 2354 bytes as ", () => {
       const output = formatBytes(1054);
-      expect(output).toBe("1 KiB");
+      expect(output).toBe("1.1 kB");
     });
   });
 });
