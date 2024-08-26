dc76b1af0185e919165f4b63516029d787a4d7f6
Lance Gliser <lance.gliser@howso.com>
2024-08-26 14:56:02 -0500

    Stupid naming

--- src/components/Icons/GithubIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { IconBaseProps } from "react-icons";
-import { FaGithub } from "react-icons/fa6";
-
-export type GitHubIconProps = IconBaseProps & {};
-export const GitHubIcon: FC<GitHubIconProps> = (props) => {
-  return <FaGithub {...props} />;
-};
