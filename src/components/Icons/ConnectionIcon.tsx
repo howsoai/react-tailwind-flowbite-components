034401a4cf589d7944e7fe8ab554d89911b43781
Lance Gliser <lance.gliser@howso.com>
2024-10-29 10:19:58 -0500

    Better name ConnectionIconProps

--- src/components/Icons/ConnectionIcon.tsx
+++ src/components/Icons/ConnectionIcon.tsx
@@ -2,10 +2,10 @@ import { type FC } from "react";
 import { type IconBaseProps } from "react-icons";
 import { HiOutlineStatusOffline, HiOutlineStatusOnline } from "react-icons/hi";
 
-type IconBaseConnectionIconProps = IconBaseProps & {
+type ConnectionIconProps = IconBaseProps & {
   status: "online" | "offline";
 };
-export const ConnectionIcon: FC<IconBaseConnectionIconProps> = ({
+export const ConnectionIcon: FC<ConnectionIconProps> = ({
   status,
   ...props
 }) =>
