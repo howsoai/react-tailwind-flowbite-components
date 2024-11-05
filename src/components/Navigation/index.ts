7550e846ed3506f9b2065d3ac2a81ee9106bc3d4
Lance Gliser <lance.gliser@howso.com>
2024-11-05 11:38:47 -0600

    added missing export

--- src/components/Navigation/index.ts
+++ src/components/Navigation/index.ts
@@ -2,6 +2,7 @@ import { I18nBundle } from "@howso/ui-internationalization-utils";
 import { Languages } from "../../constants";
 import { PaginationI18nBundle } from "./Pagination/Pagination.i18n";
 
+export * from "./Pagination";
 export * from "./Stepper";
 
 export const ComponentsNavigationI18nBundles: I18nBundle<Languages, any>[] = [
