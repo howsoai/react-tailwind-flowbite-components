7484d06a72fbf31f3e40ffe04969c65133586a52
Lance Gliser <lance.gliser@howso.com>
2024-11-06 11:11:19 -0600

    Added ViewIcon

---
+++ src/components/Icons/ViewIcon.tsx
@@ -0,0 +1,5 @@
+import { type FC } from "react";
+import { type IconBaseProps } from "react-icons";
+import { HiEye } from "react-icons/hi";
+
+export const ViewIcon: FC<IconBaseProps> = (props) => <HiEye {...props} />;
