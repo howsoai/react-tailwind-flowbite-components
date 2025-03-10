c9ce500ea70e0f3e7f1cef4ed6ec75fdc85a8e13
Lance Gliser <lance.gliser@howso.com>
2025-03-10 14:21:53 -0500

    Added storybook isDarkBackground util

--- .storybook/constants.ts
+++ .storybook/constants.ts
@@ -2,3 +2,13 @@ export const longestPersonalName =
   "Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.";
 export const longestContentName =
   "Large-area InP based photodiode operated at 850 nm wavelengths with high efficiency and high speed for 40 Gbit/sec transmission";
+
+export const lightBackground = {
+  name: "Light",
+  value: "#fff",
+};
+
+export const darkBackground = {
+  name: "Dark",
+  value: "#000",
+};
