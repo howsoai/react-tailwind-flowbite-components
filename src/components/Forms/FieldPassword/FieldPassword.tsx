4d150a9323dc494dee697a3c681ca746053d7709
Lance Gliser <lance.gliser@howso.com>
2025-03-11 08:55:34 -0500

    FieldPasswordToggleAddon tabindex-1

--- src/components/Forms/FieldPassword/FieldPassword.tsx
+++ src/components/Forms/FieldPassword/FieldPassword.tsx
@@ -109,7 +109,12 @@ export const FieldPasswordToggleAddon: FC<FieldPasswordToggleProps> = ({
     ? i18n.strings.visibility.hide
     : i18n.strings.visibility.show;
   return (
-    <button type="button" onClick={onToggle} className="m-[-12px] p-[12px]">
+    <button
+      type="button"
+      onClick={onToggle}
+      className="m-[-12px] p-[12px]"
+      tabIndex={-1}
+    >
       {<VisibilityIcon isVisible={!isVisible} title={t(title)} />}
     </button>
   );
