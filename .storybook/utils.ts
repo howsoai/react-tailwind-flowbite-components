581464564db993c43eb3ff6c309d90d92bba116a
Lance Gliser <lance.gliser@howso.com>
2025-04-15 11:07:14 -0500

    Fixed storybook dark mode detection bugs

--- .storybook/utils.ts
+++ .storybook/utils.ts
@@ -1,5 +1,20 @@
 import { StoryContext } from "@storybook/react";
 import { darkBackground } from "./constants";
 
-export const isDarkBackground = (context: StoryContext<any>): boolean =>
-  context.globals?.backgrounds?.value === darkBackground.value;
+export const isDarkBackground = (context: StoryContext<any>): boolean => {
+  console.info(
+    "context.globals?.backgrounds?.value",
+    context.globals?.backgrounds?.value,
+  );
+  console.info(
+    "context.userGlobals?.backgrounds?.value",
+    context.userGlobals?.backgrounds?.value,
+  );
+  console.info("darkBackground", darkBackground);
+  const isDark =
+    context.globals?.backgrounds?.value === darkBackground.name ||
+    context.userGlobals?.backgrounds?.value === darkBackground.name;
+  console.info("is dark", isDark);
+
+  return isDark;
+};
