96f550fa00aaf73d50f0a1a8ea33730258247db4
Lance Gliser <lance.gliser@howso.com>
2025-04-15 08:08:50 -0500

    Added StopIcon square property

--- src/components/Icons/StopIcon.tsx
+++ src/components/Icons/StopIcon.tsx
@@ -1,5 +1,13 @@
 import { type FC } from "react";
 import { type IconBaseProps } from "react-icons";
-import { HiStop } from "react-icons/hi";
+import { FaStop, FaStopCircle } from "react-icons/fa";
 
-export const StopIcon: FC<IconBaseProps> = (props) => <HiStop {...props} />;
+export type StopIconProps = IconBaseProps & {
+  /**
+   * Activates the alternative format of a square, without containing circle.
+   * Typically used in <IconButton />
+   **/
+  square?: boolean;
+};
+export const StopIcon: FC<StopIconProps> = ({ square, ...props }) =>
+  square ? <FaStop {...props} /> : <FaStopCircle {...props} />;
