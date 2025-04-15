07d1fb7778ee578809189b2cd37ccaea7dc70354
Lance Gliser <lance.gliser@howso.com>
2025-04-15 09:05:56 -0500

    Modified ContextMenu to use IconButton

--- src/components/Interaction/ContextMenu/ContextMenu.tsx
+++ src/components/Interaction/ContextMenu/ContextMenu.tsx
@@ -1,12 +1,13 @@
-import { ComponentProps, FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
+import { IconButton, IconButtonProps } from "../../Buttons";
 import { ContextMenuIcon } from "../../Icons";
 import { DropdownPortal } from "../../Navigation";
 import { ContextMenuI18nBundle as i18n } from "./ContextMenu.i18n";
 
 export type ContextMenuProps = {
-  buttonProps?: Partial<ComponentProps<"button">>;
+  buttonProps?: IconButtonProps;
   isLoading?: boolean;
   /** Expected to be a ReactNode comprised of <Dropdown.* /> items */
   children: ReactNode;
@@ -21,18 +22,17 @@ export const ContextMenu: FC<ContextMenuProps> = ({
   return (
     <DropdownPortal
       label={
-        <button
+        <IconButton
           title={t(i18n.strings.menu)}
           {...buttonProps}
           disabled={isLoading || buttonProps?.disabled}
           className={twMerge(
-            "p-1",
             buttonProps?.className,
             isLoading && "cursor-not-allowed opacity-40",
           )}
         >
           <ContextMenuIcon />
-        </button>
+        </IconButton>
       }
     >
       {children}
