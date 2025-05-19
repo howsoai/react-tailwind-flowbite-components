5819fec34943485c3757f1709449232b4d90d092
Lance Gliser <lance.gliser@howso.com>
2025-05-19 13:29:03 -0500

    23739: Added ShowWorkIcon, MINOR (#98)

---
+++ src/components/Icons/ShowWorkIcon.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+import { type IconBaseProps } from "react-icons";
+import { TbMathFunction } from "react-icons/tb";
+
+export const ShowWorkIcon: FC<IconBaseProps> = (props) => {
+  return <TbMathFunction {...props} />;
+};
